0b8b72ecb8c7bddcd13c7cbee44000f494e1bb23
anchellon <anshulmathew@gmail.com>
2023-05-07 19:11:46 -0700

    Added sessions to the boiler plate

--- app.ts
+++ app.ts
@@ -3,11 +3,38 @@ import express, { Application, Request, Response, NextFunction } from "express";
 import * as path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
+import mongoose from "mongoose";
+// Import session library from express-sessions
+import session from "express-session";
+//Import mongodb store libraries that connect to session
+import { default as connectMongoDBSession } from "connect-mongodb-session";
+
+const dbString = "mongodb://localhost:27017";
+
+const MongoDBStore = connectMongoDBSession(session);
+
+const sessionStore = new MongoDBStore({
+  uri: dbString,
+  databaseName: "cloudcamp",
+  collection: "sessions",
+});
+
 let indexRouter = require("./routes/index");
 let usersRouter = require("./routes/users");
 
-var app: Application = express();
+let app: Application = express();
 
+app.use(
+  session({
+    secret: "some secret",
+    resave: false,
+    saveUninitialized: true,
+    store: sessionStore,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24, // Equals 1 day (1 day * 24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms / 1 sec)
+    },
+  })
+);
 // view engine setup
 
 app.use(logger("dev"));
---
+++ src/db/mongoConn.ts
@@ -0,0 +1,2 @@
+// docker run -d -p 27017:27017 --name mongo-dave mongo:latest
+import mongoose from "mongoose";
