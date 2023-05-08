c57ec7c67f73fe7044965020152790f70d2ff9a8
anchellon <anshulmathew@gmail.com>
2023-05-07 18:19:25 -0700

    Added comments to added typed def of sessionss

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,13 @@
 import { Request, Response, NextFunction, Router } from "express";
+// Extending Session Type definition to include our required session data
+// import express from "express";
+// import User from "./User";
+
+// declare module "express-session" {
+//   interface SessionData {
+//     user: User;
+//   }
+// }
 
 let router = Router();
 
