ec1222283fd54abfb56c9e5b5659200442a82414
anchellon <anshulmathew@gmail.com>
2023-05-15 17:19:37 -0700

    Cleaned up code

--- app.ts
+++ app.ts
@@ -7,22 +7,16 @@ import mongoose from "mongoose";
 import session from "express-session";
 //Import mongodb store libraries that connect to session
 import { default as connectMongoDBSession } from "connect-mongodb-session";
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 // Creating passport connection
 
 import passport from "passport";
 import dotenv from "dotenv";
-import User from "./src/models/User";
+
+const MongoDBStore = connectMongoDBSession(session);
 
 dotenv.config();
 const dbString: any = process.env.DB_STRING;
-const googleCientID: any = process.env.GOOGLE_CLIENTID;
-const googleCientSecret: any = process.env.GOOGLE_SECRET;
-const ghCientID: any = process.env.GH_CLIENTID;
-const ghCientSecret: any = process.env.GH_SECRET;
-const MongoDBStore = connectMongoDBSession(session);
-
 const sessionStore = new MongoDBStore({
   uri: dbString,
   databaseName: "cloudcamp",
@@ -39,13 +33,9 @@ sessionStore.on("error", function (error) {
 });
 
 // let indexRouter = require("./src/routes/index");
-// let usersRouter = require("./src/routes/users");
+let usersRouter = require("./src/routes/users");
 
 let app: Application = express();
-
-// app.use("/", indexRouter);
-// app.use("/users", usersRouter);
-
 app.use(
   session({
     secret: "some secret",
@@ -57,72 +47,14 @@ app.use(
     },
   })
 );
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(passport.initialize());
 app.use(passport.session());
-// add strategy below session
-passport.serializeUser((user: any, done: any) => {
-  console.log(user._id);
-  done(null, user._id);
-});
-passport.deserializeUser((id: any, done) => {
-  console.log(id);
-  console.log("throw up");
-  User.findById(id).then((user) => {
-    done(null, user);
-  });
-});
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: googleCientID,
-      clientSecret: googleCientSecret,
-      callbackURL: "http://localhost:3000/users/auth/google/callback",
-    },
-    // Save the user here
-    function (accessToken: any, refreshToken: any, profile: any, done: any) {
-      User.findOne({ id: profile.id, provider: "google" }).then((currUser) => {
-        if (currUser) {
-          console.log("current user ", currUser);
-          done(null, currUser);
-        } else {
-          new User({
-            username: "",
-            hash: "",
-            salt: "",
-            firstName: profile.name.givenName,
-            lastName: profile.name.familyName,
-            profilePic: profile.photos[0].value,
-            id: profile.id,
-            provider: "google",
-            isAdmin: false,
-            role: "user",
-          })
-            .save()
-            .then((newUser) => {
-              console.log("New user created" + newUser);
-              done(null, newUser);
-            });
-        }
-      });
-    }
-  )
-);
-
-app.get(
-  "/users/auth/google",
-  passport.authenticate("google", { scope: ["profile"] })
-);
-app.get(
-  "/users/auth/google/callback",
-  passport.authenticate("google"),
-  function (req, res) {
-    // Successful authentication, redirect home.
-    console.log(req.user);
-    res.redirect("http://localhost:5173");
-  }
-);
+app.use("/users", usersRouter);
 
 app.get("/logout", function (req, res, next) {
   console.log(req.user);
@@ -138,62 +70,26 @@ app.get("/logout", function (req, res, next) {
   }
 });
 
-//  Next strategy
-// passport.use(
-//   "github-strat",
-//   new OAuth2Strategy(
-//     {
-//       authorizationURL: "https://github.com/login/oauth/authorize",
-//       tokenURL: "https://github.com/login/oauth/access_token",
-//       clientID: ghCientID,
-//       clientSecret: ghCientSecret,
-//       callbackURL: "http://localhost:3000/auth/github/callback",
-//     },
-//     // Called on successful login , use logic here like insert into db
-//     function (accessToken: any, refreshToken: any, profile: any, cb: any) {
-//       console.log(profile);
-//       cb(null, profile);
-//     }
-//     // default example call back
-//     // function (accessToken: , refreshToken, profile, cb) {
-//     //   User.findOrCreate({ exampleId: profile.id }, function (err, user) {
-//     //     return cb(err, user);
-//     //   });
-//     // }
-//   )
-// );
-// app.use(logger("dev"));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-
 // app.use(express.static(path.join(__dirname, "public")));
 
 // catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//   next(createError(404));
-// });
+app.use(function (req, res, next) {
+  next(createError(404));
+});
 
 // error handler
-// app.use(function (err: Error, req: Request, res: Response, next: NextFunction) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get("env") === "development" ? err : {};
-//   let errorStatus = 0;
-//   // render the error page
-//   res.status(errorStatus || 500).send();
-//   // res.render("error");
-// });
+app.use(function (err: Error, req: Request, res: Response, next: NextFunction) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+  let errorStatus = 0;
+  // render the error page
+  res.status(errorStatus || 500).send();
+  // res.render("error");
+});
 
 app.listen(3000, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${3000}`);
 });
 
-// app.get(
-//   "/auth/github/callback",
-//   passport.authenticate("github-strat ", { failureRedirect: "/login" }),
-//   function (req, res) {
-//     // Successful authentication, redirect home.
-//     res.redirect("http://localhost:5173");
-//   }
-// );
 module.exports = app;
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,10 +1,117 @@
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+import passport = require("passport");
+import dotenv = require("dotenv");
+
+import User from "../models/User";
 import { Request, Response, NextFunction, Router } from "express";
+import OAuth2Strategy = require("passport-oauth2");
 
 let router = Router();
 
-/* GET users listing. */
-router.get("/", function (req: Request, res: Response, next: NextFunction) {
-  res.send("respond with a resource");
+dotenv.config();
+const googleCientID: any = process.env.GOOGLE_CLIENTID;
+const googleCientSecret: any = process.env.GOOGLE_SECRET;
+const ghCientID: any = process.env.GH_CLIENTID;
+const ghCientSecret: any = process.env.GH_SECRET;
+passport.serializeUser((user: any, done: any) => {
+  console.log(user._id);
+  done(null, user._id);
+});
+passport.deserializeUser((id: any, done) => {
+  console.log(id);
+  console.log("throw up");
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
 });
 
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: googleCientID,
+      clientSecret: googleCientSecret,
+      callbackURL: "http://localhost:3000/users/auth/google/callback",
+    },
+    // Save the user here
+    function (accessToken: any, refreshToken: any, profile: any, done: any) {
+      User.findOne({ id: profile.id, provider: "google" }).then((currUser) => {
+        if (currUser) {
+          console.log("current user ", currUser);
+          done(null, currUser);
+        } else {
+          new User({
+            username: "",
+            hash: "",
+            salt: "",
+            firstName: profile.name.givenName,
+            lastName: profile.name.familyName,
+            profilePic: profile.photos[0].value,
+            id: profile.id,
+            provider: "google",
+            isAdmin: false,
+            role: "user",
+          })
+            .save()
+            .then((newUser) => {
+              console.log("New user created" + newUser);
+              done(null, newUser);
+            });
+        }
+      });
+    }
+  )
+);
+
+passport.use(
+  "github-strat",
+  new OAuth2Strategy(
+    {
+      authorizationURL: "https://github.com/login/oauth/authorize",
+      tokenURL: "https://github.com/login/oauth/access_token",
+      clientID: ghCientID,
+      clientSecret: ghCientSecret,
+      callbackURL: "http://localhost:3000/auth/github/callback",
+    },
+    // Called on successful login , use logic here like insert into db
+    function (accessToken: any, refreshToken: any, profile: any, cb: any) {
+      console.log(profile);
+      cb(null, profile);
+    }
+    // default example call back
+    // function (accessToken: , refreshToken, profile, cb) {
+    //   User.findOrCreate({ exampleId: profile.id }, function (err, user) {
+    //     return cb(err, user);
+    //   });
+    // }
+  )
+);
+
+/* GET users listing. */
+router.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile"] })
+);
+router.get(
+  "/auth/google/callback",
+  passport.authenticate("google"),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    console.log(req.user);
+    res.redirect("http://localhost:5173");
+  }
+);
+
+router.get(
+  "/auth/github",
+  passport.authenticate("ouath2", { scope: ["profile"] })
+);
+
+router.get(
+  "/auth/github/callback",
+  passport.authenticate("github-strat ", { failureRedirect: "/login" }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("http://localhost:5173");
+  }
+);
 module.exports = router;
