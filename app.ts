2adb1a7e13ffa440ad2bcb7b3b9dd2d6d9fc9b93
anchellon <anshulmathew@gmail.com>
2023-05-28 19:30:25 -0700

    cookie modd

--- app.ts
+++ app.ts
@@ -51,7 +51,7 @@ app.use(
     saveUninitialized: false,
     store: sessionStore,
     cookie: {
-      secure: false,
+      sameSite: "none",
       maxAge: 1000 * 60 * 60 * 24, // Equals 1 day (1 day * 24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms / 1 sec)
     },
   })
